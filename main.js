18d4aecf8f4713b66d19280292851130e3cbeca6
Christian Adler <chris.adler@gmx.de>
2024-05-12 07:23:24 +0200

    add dot following path animation

--- main.js
+++ main.js
@@ -1,14 +1,64 @@
 const svg = document.querySelector('#svg');
-const btn = document.querySelector('#btn');
-let svgLoaded = false;
+const btnChangeText = document.querySelector('#btnChangeText');
+const btnAnimate = document.querySelector('#btnAnimate');
+
+let svgDoc = null;
+
+let path = null;
+let dot = null;
+
 svg.onload = function () {
   console.log('SVG loaded!');
-  svgLoaded = true;
+  svgDoc = svg.contentDocument;
+
+  path = svgDoc.querySelector('#pathID');
+  dot = svgDoc.querySelector('#dotID');
+
+  clear();
 }
 
-btn.addEventListener('click', (/*e*/) => {
-  if (!svgLoaded)
+btnChangeText.addEventListener('click', (/*e*/) => {
+  if (!svgDoc)
     return;
-  const doc = svg.contentDocument;
-  doc.querySelector('#textID').textContent = "some new value";
+  svgDoc.querySelector('#textID').textContent = "some new value";
 });
+
+let animating = false;
+btnAnimate.addEventListener('click', (/*e*/) => {
+  if (!svgDoc)
+    return;
+
+  animating = !animating;
+  btnAnimate.innerText = animating ? 'Stop animation' : 'Start animation';
+
+  if (animating) {
+    fill();
+    animate();
+  }
+});
+
+
+function clear() {
+  dot.style.fill = 'none';
+}
+
+function fill() {
+  dot.style.fill = '#f00';
+}
+
+let step = 0;
+const steps = 100;
+
+function animate() {
+  const len = path.getTotalLength();
+  const pointOnPath = path.getPointAtLength(len / steps * step);
+  dot.setAttribute('cx', pointOnPath.x);
+  dot.setAttribute('cy', pointOnPath.y);
+  step++;
+  if (step > steps) step = 0; // reset
+
+  if (animating)
+    requestAnimationFrame(animate);
+  else
+    clear();
+}
